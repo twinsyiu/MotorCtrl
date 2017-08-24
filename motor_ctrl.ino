24bcb70a781d27119e46898bc9f14ee1089943bb
tyiu-VAIO\t yiu <kendotwins@gmail.com>
2017-08-24 13:32:47 +1000

    Motor Control Pins are using CONSTANTS rather than variables, supposing it should be tested locally by TY. Now leave the testing to MY.

--- motor_ctrl.ino
+++ motor_ctrl.ino
@@ -1,31 +1,25 @@
-/*
+// Motor Control using constant PIN NAME
+// motor one
+#define ENA 6
 #define IN1 4
 #define IN2 5
+// motor two
+#define ENB 9
 #define IN3 7
 #define IN4 8
-#define ENA 6
-#define ENB 9
+// motor names
 #define MTR_L 0
 #define MTR_R 1
-*/
-// motor one
-int enA = 6;
-int in1 = 4;
-int in2 = 5;
-// motor two
-int enB = 9;
-int in3 = 7;
-int in4 = 8;
 
 void setup()
 {
   // set all the motor control pins to outputs
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 void move_motor(boolean motor, int motor_speed, int motor_direction) 
 {
@@ -36,27 +30,27 @@ void demoOne()
 {
   // this function will run the motors in both directions at a fixed speed
   // turn on motor A
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   // set speed to 200 out of possible range 0~255
-  analogWrite(enA, 200);
+  analogWrite(ENA, 200);
   // turn on motor B
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
   // set speed to 200 out of possible range 0~255
-  analogWrite(enB, 200);
+  analogWrite(ENB, 200);
   delay(2000);
   // now change motor directions
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH); 
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);  
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); 
   delay(2000);
   // now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);  
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 void loop() {
